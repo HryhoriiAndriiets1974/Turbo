7bcee069a3c55fe88ae770a4d8382e48ab9b9992
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-02-21 16:36:40 +0200

    dz-4-33

--- js/script-4.js
+++ js/script-4.js
@@ -800,5 +800,194 @@
 //   console.log(getUserWithEmail(users, "elmahead@omatom.com"));
 //   console.log(getUserWithEmail(users, "blackburndotson@furnigeer.com"));
 //===============================================---- 30
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every((value) => (value % 2) === 0);
+// const eachElementInFirstIsOdd = firstArray.every((value) => (value % 2) === 1);
+
+// const eachElementInSecondIsEven = secondArray.every((value) => (value % 2) === 0);
+// const eachElementInSecondIsOdd = secondArray.every((value) => (value % 2) === 1);
+
+// const eachElementInThirdIsEven = thirdArray.every((value) => (value % 2) === 0);
+// const eachElementInThirdIsOdd = thirdArray.every((value) => (value % 2) === 1);
+// console.log(eachElementInFirstIsEven);
+// console.log(eachElementInFirstIsOdd);
+// console.log(eachElementInSecondIsEven);
+// console.log(eachElementInSecondIsOdd);
+// console.log(eachElementInThirdIsEven);
+// console.log(eachElementInThirdIsOdd);
 //===============================================---- 31
+// // Change code below this line
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ]
+// const isEveryUserActive = (users) => {
+//   return users.every((user) => user.isActive);
+//   };
+//   // Change code above this line
+//   console.log(isEveryUserActive(users));
 //===============================================---- 32
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
+
+// const anyElementInFirstIsEven = firstArray.some((value) => (value % 2 === 0));
+// const anyElementInFirstIsOdd = firstArray.some((value) => (value % 2 === 1));
+
+// const anyElementInSecondIsEven = secondArray.some((value) => (value % 2 === 0));
+// const anyElementInSecondIsOdd = secondArray.some((value) => (value % 2 === 1));
+
+// const anyElementInThirdIsEven = thirdArray.some((value) => (value % 2 === 0));
+// const anyElementInThirdIsOdd = thirdArray.some((value) => (value % 2 === 1));
+// console.log(anyElementInFirstIsEven);
+// console.log(anyElementInFirstIsOdd);
+// console.log(anyElementInSecondIsEven);
+// console.log(anyElementInSecondIsOdd);
+// console.log(anyElementInThirdIsEven);
+// console.log(anyElementInThirdIsOdd);
+//===============================================---- 33
+// // Change code below this line
+// const users = [
+// {
+//   name: "Moore Hensley",
+//   email: "moorehensley@indexia.com",
+//   eyeColor: "blue",
+//   friends: ["Sharron Pace"],
+//   isActive: false,
+//   balance: 2811,
+//   gender: "male"
+// },
+// {
+//   name: "Sharlene Bush",
+//   email: "sharlenebush@tubesys.com",
+//   eyeColor: "blue",
+//   friends: ["Briana Decker", "Sharron Pace"],
+//   isActive: true,
+//   balance: 3821,
+//   gender: "female"
+// },
+// {
+//   name: "Ross Vazquez",
+//   email: "rossvazquez@xinware.com",
+//   eyeColor: "green",
+//   friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//   isActive: false,
+//   balance: 3793,
+//   gender: "male"
+// },
+// {
+//   name: "Elma Head",
+//   email: "elmahead@omatom.com",
+//   eyeColor: "green",
+//   friends: ["Goldie Gentry", "Aisha Tran"],
+//   isActive: true,
+//   balance: 2278,
+//   gender: "female"
+// },
+// {
+//   name: "Carey Barr",
+//   email: "careybarr@nurali.com",
+//   eyeColor: "blue",
+//   friends: ["Jordan Sampson", "Eddie Strong"],
+//   isActive: true,
+//   balance: 3951,
+//   gender: "male"
+// },
+// {
+//   name: "Blackburn Dotson",
+//   email: "blackburndotson@furnigeer.com",
+//   eyeColor: "brown",
+//   friends: ["Jacklyn Lucas", "Linda Chapman"],
+//   isActive: false,
+//   balance: 1498,
+//   gender: "male"
+// },
+// {
+//   name: "Sheree Anthony",
+//   email: "shereeanthony@kog.com",
+//   eyeColor: "brown",
+//   friends: ["Goldie Gentry", "Briana Decker"],
+//   isActive: true,
+//   balance: 2764,
+//   gender: "female"
+// }
+// ]
+// const isAnyUserActive = users => {
+//   return users.some((value) => value.isActive);
+//   };
+//   // Change code above this line
+//   console.log(isAnyUserActive(users));
+//===============================================---- 34
+
+//===============================================---- 35
+//===============================================---- 36
+//===============================================---- 37
+//===============================================---- 38
+//===============================================---- 39
+//===============================================---- 40
