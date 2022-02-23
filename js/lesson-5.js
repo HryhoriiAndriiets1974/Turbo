5950ba88b6632970a9af7a578dda3dbf610f0b2b
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-02-23 19:32:16 +0200

    condition of the problem 1-48

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -1,111 +1,113 @@
 
-// 6. Напиши скрипт управления личным кабинетом интернет банка.
-// Есть объект account в котором необходимо реализовать методы
-// для работы с балансом и историей транзакций.
+// // 6. Напиши скрипт управления личным кабинетом интернет банка.
+// // Есть объект account в котором необходимо реализовать методы
+// // для работы с балансом и историей транзакций.
 
 
-/*
- * Типов транзацкий всего два.
- * Можно положить либо снять деньги со счета.
- */
-const Transaction = {
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw"
-};
+// /*
+//  * Типов транзацкий всего два.
+//  * Можно положить либо снять деньги со счета.
+//  */
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw"
+// };
 
-/*
- * Каждая транзакция это объект со свойствами: id, type и amount
- */
+// /*
+//  * Каждая транзакция это объект со свойствами: id, type и amount
+//  */
 
-const account = {
-  // Текущий баланс счета
-  balance: 0,
+// const account = {
+//   // Текущий баланс счета
+//   balance: 0,
 
-  // История транзакций
-  transactions: [],
-  id: 0,
+//   // История транзакций
+//   transactions: [],
+//   id: 0,
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {
-    return { amount, type, id: (this.id += 1) };
-  },
+//   /*
+//    * Метод создает и возвращает объект транзакции.
+//    * Принимает сумму и тип транзакции.
+//    */
+//   createTransaction(amount, type) {
+//     return { amount, type, id: (this.id += 1) };
+//   },
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {
-    this.balance += amount;
-    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
-    this.transactions.push(transaction);
-  },
+//   /*
+//    * Метод отвечающий за добавление суммы к балансу.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций
+//    */
+//   deposit(amount) {
+//     this.balance += amount;
+//     const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.transactions.push(transaction);
+//   },
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {
-    if (amount > this.balance) {
-      return "Недостаточно средств";
-    } else {
-      this.balance -= amount;
-      const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
-      this.transactions.push(transaction);
-    }
-  },
+//   /*
+//    * Метод отвечающий за снятие суммы с баланса.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций.
+//    *
+//    * Если amount больше чем текущий баланс, выводи сообщение
+//    * о том, что снятие такой суммы не возможно, недостаточно средств.
+//    */
+//   withdraw(amount) {
+//     if (amount > this.balance) {
+//       return "Недостаточно средств";
+//     } else {
+//       this.balance -= amount;
+//       const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//       this.transactions.push(transaction);
+//     }
+//   },
 
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {
-    return this.balance;
-  },
+//   /*
+//    * Метод возвращает текущий баланс
+//    */
+//   getBalance() {
+//     return this.balance;
+//   },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {
-    for (const transaction of this.transactions) {
-      if (transaction.id === id) return transaction;
-    }
-  },
+//   /*
+//    * Метод ищет и возвращает объект транзации по id
+//    */
+//   getTransactionDetails(id) {
+//     for (const transaction of this.transactions) {
+//       if (transaction.id === id) return transaction;
+//     }
+//   },
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  getTransactionTotal(type) {
-    const transactionTyped = [];
-    for (const transaction of this.transactions) {
-      if (transaction.type === type) {
-        transactionTyped.push(transaction);
-      }
-    }
+//   /*
+//    * Метод возвращает количество средств
+//    * определенного типа транзакции из всей истории транзакций
+//    */
+//   getTransactionTotal(type) {
+//     const transactionTyped = [];
+//     for (const transaction of this.transactions) {
+//       if (transaction.type === type) {
+//         transactionTyped.push(transaction);
+//       }
+//     }
 
-    return transactionTyped;
-  }
-};
+//     return transactionTyped;
+//   }
+// };
 
-account.deposit(1500);
-account.deposit(500);
-account.deposit(2500);
-console.log(account.getBalance());
-console.log(account.transactions);
-account.withdraw(500);
-account.withdraw(100);
-account.withdraw(1500);
-console.log(account.getBalance());
-console.log(account.transactions);
-console.log(account.getTransactionDetails(2));
-
-console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+// account.deposit(1500);
+// account.deposit(500);
+// account.deposit(2500);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+// account.withdraw(500);
+// account.deposit(2500);
+// account.withdraw(1500);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+// console.log(account.getTransactionDetails(2));
+// account.deposit(2500);
+// account.deposit(2500);
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+// console.log(account.getBalance());
--- js/script-4.js
+++ js/script-4.js
@@ -1,6 +1,9 @@
 "use strict";
 // console.dir(Object);
 //===========================---- 1
+// Доповни код таким чином, щоб у змінній result був
+// результат виконання функції makePizza, а у змінній
+// pointer було посилання на функцію makePizza.
 // function makePizza() {
 //   return "Your pizza is being prepared, please wait.";
 // }
@@ -9,6 +12,12 @@
 // const result = makePizza();
 // const pointer = makePizza;
 //===============================================---- 2
+// Доповни функцію makeMessage таким чином, щоб вона
+// очікувала другим параметром (параметр callback)
+// колбек-функцію і повертала її виклик. Функція
+// deliverPizza або makePizza буде передаватися як
+// колбек і очікувати аргументом ім'я готової піци,
+//  що доставляється.
 // function deliverPizza(pizzaName) {
 //   return `Delivering ${pizzaName} pizza.`;
 // }
@@ -22,6 +31,10 @@
 //   return callback(pizzaName);
 // }
 //===============================================---- 3
+// Доповни другий виклик функції makePizza(pizzaName,
+//  callback), передавши другим аргументом інлайн
+// колбек-функцію eatPizza(pizzaName), яка логує рядок
+// "Eating pizza <назва піци>".
 // function makePizza(pizzaName, callback) {
 //   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
 //   callback(pizzaName);
@@ -36,6 +49,21 @@
 //   console.log(`Eating pizza ${pizzaName}`)
 // });
 //===============================================---- 4
+// Необхідно написати логіку обробки замовлення піци.
+// Виконай рефакторинг методу order таким чином, щоб
+// він приймав другим і третім параметром два колбеки
+//  onSuccess і onError.
+// Якщо у властивості pizzas відсутня піца з назвою з
+//  параметра pizzaName, метод order повинен повертати
+//  результат виклику колбека onError, передаючи йому
+//  аргументом рядок "There is no pizza with a
+// name{/* <имя пиццы> in the assortment." */}
+// Якщо у властивості pizzas присутня піца з назвою
+// з параметра pizzaName, метод order повинен повертати
+//  результат виклику колбека onSuccess, передаючи йому
+//  аргументом назву замовленої піци.
+// Після оголошення об'єкта pizzaPalace ми додали
+// колбеки і виклики методів. Будь ласка, нічого там не міняй.
 // const pizzaPalace = {
 //   pizzas: ["Ultracheese", "Smoked", "Four meats"],
 //   order(pizzaName, onSuccess, onError) {
@@ -65,6 +93,7 @@
 // pizzaPalace.order("Big Mike", makePizza, onOrderError);
 // pizzaPalace.order("Vienna", makePizza, onOrderError);
 //===============================================---- 5
+
 // function calculateTotalPrice(orderedItems) {
 //   let totalPrice = 0;
 //   // Change code below this line
@@ -77,6 +106,12 @@
 //   return totalPrice;
 // }
 //===============================================---- 5
+// Функція calculateTotalPrice(orderedItems) приймає
+//  один параметр orderedItems - масив чисел, і розраховує
+//  загальну суму його елементів, яка зберігається у
+// змінній totalPrice і повертається як результат роботи функції.
+// Виконай рефакторинг функції таким чином, щоб замість
+// циклу for, вона використовувала метод forEach.
 // function calculateTotalPrice(orderedItems) {
 //   let totalPrice = 0;
 //   // Change code below this line
@@ -89,6 +124,12 @@
 //   return totalPrice;
 // }
 //===============================================---- 6
+// Функція filterArray(numbers, value) приймає масив чисел
+//  numbers і повертає новий масив, в якому будуть тільки
+//  ті елементи оригінального масиву, які більші за
+// значення параметра value.
+// Виконай рефакторинг функції таким чином, щоб замість
+// циклу for, вона використовувала метод forEach.
 // function filterArray(numbers, value) {
 //   const filteredNumbers = [];
 //   // Change code below this line
@@ -103,6 +144,13 @@
 //   return filteredNumbers;
 // }
 //===============================================---- 7
+// Функція getCommonElements(firstArray, secondArray)
+// приймає два масиви довільної довжини в параметри
+// firstArray і secondArray, і повертає новий масив
+// їхніх спільних елементів, тобто тих, які присутні
+//  в обох масивах.
+// Виконай рефакторинг функції таким чином, щоб замість
+//  циклу for, вона використовувала метод forEach.
 // function getCommonElements(firstArray, secondArray) {
 //   const commonElements = [];
 //   // Change code below this line
@@ -117,11 +165,15 @@
 //   // Change code above this line
 // }
 //===============================================---- 8
+// Виконай рефакторинг функції calculateTotalPrice()
+// таким чином, щоб вона була оголошена як стрілочна.
 // const calculateTotalPrice = (quantity, pricePerItem) => {
 //     // Change code above this line
 //     return quantity * pricePerItem;
 //   }
 //===============================================---- 9
+// Виконай рефакторинг функції calculateTotalPrice()
+// таким чином, щоб вона використовувала неявне повернення.
 // Change code below this line
 // const calculateTotalPrice = (quantity, pricePerItem) =>  quantity * pricePerItem;
 
