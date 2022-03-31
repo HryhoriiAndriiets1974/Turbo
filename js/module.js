6179b68e8539a2cff179ba361a73a6dd8a7de0f8
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-03-31 16:13:51 +0300

    mod-4-example-2

--- js/module.js
+++ js/module.js
@@ -26,10 +26,125 @@
 // console.log(newArr2);
 // ====== every ====
 // every під капотом
-const numbers = [1, -2, 3]; // every > 0
+// const numbers = [1, -2, 3]; // every > 0
 
-if (numbers[0] > 0 && numbers[1] > 0 && numbers[2] > 0) {
-  console.log(true);
-} else {
-  console.log(false);
-}
+// if (numbers[0] > 0 && numbers[1] > 0 && numbers[2] > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// ====== some ====
+// some під капотом
+// const numbers = [1, -2, 3]; // every > 0
+
+// if (numbers[0] > 0 || numbers[1] > 0 || numbers[2] > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// ========================
+// ======= examples =======
+// ========================
+const cars = [
+  {
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+  {
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
+console.table(cars);
+// ====================================
+// Example 1 - Метод map
+// Пусть функция getModels возвращает массив моделей (поле model) всех автомобилей.
+// const getModels = (cars) => cars.map(({ model }) => model);
+
+// console.table(getModels(cars));
+// ====================================
+// Example 2 - Метод map
+// Пусть функция makeCarsWithDiscount возвращает новый массив объектов с изменным значением свойства price в зависимости от переданной скидки.
+// const makeCarsWithDiscount = (cars, discount) =>
+//   cars.map((car) => ({
+//     ...car,
+//     price: car.price - car.price * discount,
+//   }));
+
+// console.table(makeCarsWithDiscount(cars, 0.2));
+// console.table(makeCarsWithDiscount(cars, 0.4));
+// console.table(makeCarsWithDiscount(cars, 0.9));
+// console.table(makeCarsWithDiscount(cars, 1));
