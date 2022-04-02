320864ac6805cc8551a7d1b1329401bcbac8b912
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-02 08:32:38 +0300

    m-04 reduce+++

--- js/module.js
+++ js/module.js
@@ -42,6 +42,43 @@
 // } else {
 //   console.log(false);
 // }
+// ============= reduce =========
+// Масив тегів масиву обєктів всіх.
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+const getTags = tweets =>
+  tweets.reduce((allTags, tweet) => {
+    allTags.push(...tweet.tags);
+
+    return allTags;
+  }, []);
+
+const tags = getTags(tweets);
+console.log(getTags(tweets));
+
+// обробка масиву всіх тегів, - обєкт що містить
+// кількість повторів кожного тега
+const getTagStats = (acc, tag) => {
+  if (!acc.hasOwnProperty(tag)) {
+    acc[tag] = 0;
+  }
+
+  acc[tag] += 1;
+
+  return acc;
+};
+
+// Початкове значення акумулятора - це порожній об'єкт {}
+const countTags = tags => tags.reduce(getTagStats, {});
+
+const tagCount = countTags(tags);
+console.log(tagCount);
 
 // ========================
 // ======= examples =======
