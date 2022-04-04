f635b6366580a676aac603c2688e9a4a220cdb92
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-04 11:41:31 +0300

    mod-05-ex-09-a

--- js/module05.js
+++ js/module05.js
@@ -8,3 +8,23 @@
 // console.log(bookShelf.getAuthors());
 // bookShelf.addAuthor("Ли Танит");
 // console.log(bookShelf.getAuthors());
+// ================
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+// ===============
+// const petya = {
+//   username: "Petya",
+//   showThis() {
+//     console.log(this);
+//   },
+//   showName() {
+//     console.log(this.username);
+//   },
+// };
+
+// petya.showThis();
+// petya.showName();
+// ===============
