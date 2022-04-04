3a9e454bf222270559ca11d89a4958257acaba89
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-04 11:33:16 +0300

    mod-05-start

---
+++ js/module05.js
@@ -0,0 +1,10 @@
+// ============== this
+// const bookShelf = {
+//   authors: ["Бернард Корнуелл", "Роберт Шеклі"],
+//   getAuthors() { return this.authors;},
+//   addAuthor(authorName) { this.authors.push(authorName);},
+// };
+
+// console.log(bookShelf.getAuthors());
+// bookShelf.addAuthor("Ли Танит");
+// console.log(bookShelf.getAuthors());
