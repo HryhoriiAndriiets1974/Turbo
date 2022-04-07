b0519b5b8b098c77c4c68cc3c91f4c5d178be9cc
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-04-07 14:10:48 +0300

    mod-05 ex-10 get_and_set

--- js/module05.js
+++ js/module05.js
@@ -27,4 +27,74 @@
 
 // petya.showThis();
 // petya.showName();
-// ===============
+// =============== геттери і сеттери
+// =============== статичні властивості і методи
+// class User {
+//   // Оголошення та ініціалізація статичної властивості
+//   static Roles = {
+//     ADMIN: "admin",
+//     EDITOR: "editor",
+//   };
+
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role }) {
+//     this.#email = email;
+//     this.#role = role;
+//     User.#takenEmails.push(email);
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+
+
+// }
+
+// const mango = new User({
+//   email: "mango1@mail.com",
+//   role: User.Roles.ADMIN,
+// });
+
+// console.log(mango.Roles); // undefined
+// console.log(User.Roles); // { ADMIN: "admin", EDITOR: "editor" }
+
+// console.log(mango.role); // "admin"
+// mango.role = User.Roles.EDITOR;
+// console.log(mango.role);
+// console.log(User.isEmailTaken("poly@mail.com"));
+// console.log(User.isEmailTaken("mango@mail.com"));
+
+// ====== Наслідування класів
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   // Тіло класу ContentEditor
+// }
+
+// const editor = new ContentEditor("mango@mail.com");
+// console.log(editor); // { email: "mango@mail.com" }
+// console.log(editor.email); // "mango@mail.com"
